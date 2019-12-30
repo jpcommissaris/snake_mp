a17a234334a4d0a45b8135efbc0612df9fbe62e2
Julian Commissaris <jpcommissaris@gmail.com>
2019-12-30 06:37:24 -0800

    working game with rendering on client side and logic on server side

--- client/js/app.js
+++ client/js/app.js
@@ -5,8 +5,6 @@ const MAX_CONNS = 8;
 let socket;
 socket = io();
 
-let game; 
-
 let canv = document.getElementById('cvs');
 let ctx = canv.getContext('2d');
 ctx.font = '28px Arial'; 
@@ -17,162 +15,72 @@ canv.height = window.innerHeight;
 let tc = [60, 40];
 let size = canv.width/(tc[0]+2)
 let gameRect = [size, size, size*tc[0], size*tc[1]];
+let playernum = -1; 
+let players = [null]; 
+
+let food = [{x: 10, y: 20}]; 
+let color = ['lightgreen', 'lightblue', 'violet', 'orange']; 
+
+socket.on('pn', (pn) => {
+    playernum = pn;
+}) 
+
+function handleGraphics(){
+    // repaint white background black playing field
+    ctx.fillStyle="white";
+    ctx.fillRect(0,0,canv.width,canv.height); 
+    ctx.fillStyle="black"; 
+    ctx.fillRect(gameRect[0], gameRect[1], gameRect[2], gameRect[3]);
+
+    //draw scroes 
+    drawScores(ctx);
+    // paint food
+    food.forEach(f => {
+        ctx.fillStyle="red"; 
+        ctx.fillRect(f.x*size,f.y*size,size,size);
+    }); 
+    // paint snake
+    let i =0; 
+    players.forEach(snake => {
+        ctx.fillStyle= color[i]; 
+        if(snake != null){
+            draw(snake, ctx)
+        }
+        i++; 
+    });
+}  
 
 
-// baseline object 
-let SnakeEX = {
-    name: "",
-    playernum: -1,
-    color: (0,0,0),
-    px: 0,
-    py: 0,
-    vx: 0, 
-    vy: 0,
-    tail: 0, 
-    trail: []
+function drawScores(ctx){
+    let text = ""
+    let i = 0; 
+    players.forEach(snake => {
+        if(snake){
+            ctx.fillStyle= color[i]; 
+            text = snake.name + "'s Score: " + (snake.tail-5); 
+            ctx.fillText(text, size*7*(i+1) -size*5, size*2); 
+        }
+        i++; 
+    })
+
+}
+function draw(snake, ctx){
+    //paint snake
+    for(let i=0; i<snake.trail.length; i++){
+        ctx.fillRect(snake.trail[i].x*size, snake.trail[i].y*size, size-2, size-2)
+    } 
+}
+
+function gameloop() {
+    socket.on('data', (data) => {
+        window.addEventListener('keydown', getDirection); 
+        players = data.players;
+        food = data.food;
+        handleGraphics(); 
+    }); 
 }
 
 
-// -- OBJECTS FOR THE GAME --
-class Game {
-    constructor(){
-        this.players = Array(MAX_CONNS).fill(null) ;
-        this.playernum = -1; ///next avaible space in list
-        this.food = []
-        this.food.push(this.createFood());
-        this.food.push(this.createFood());
-    }
-
-    getMe(){
-        return this.players[this.playernum]; 
-    }
-
-    handleNetwork() {
-        socket.on('data', (data) => {
-            let pn = 0; 
-            Object.values(data).forEach(snake => {
-                pn = snake.playernum;
-                this.players[pn] = snake;
-                //this.playernum = pn
-              });
-        }); 
-        if(game.getMe()){
-            socket.emit('datarequest', game.getMe()); //send data to server
-        }
-    }
-
-    handleLogic() {
-        this.players.forEach(snake => {
-            if(snake){
-                this.move(snake);
-                this.checkFood(snake); 
-                if(this.checkDeath(snake)){
-                    console.log('dead', snake.px, snake.py);
-                    this.respawn(snake);
-                }
-            }
-        });
-    } 
-    handleGraphics(){
-        // repaint white background black playing field
-        ctx.fillStyle="white";
-        ctx.fillRect(0,0,canv.width,canv.height); 
-        ctx.fillStyle="black"; 
-        ctx.fillRect(gameRect[0], gameRect[1], gameRect[2], gameRect[3]);
-
-        //draw scroes 
-        this.drawScores(ctx);
-        // paint food
-        this.food.forEach(f => {
-            ctx.fillStyle="red"; 
-            ctx.fillRect(f.x*size,f.y*size,size,size);
-        }); 
-        // paint snake
-        this.players.forEach(snake => {
-            if(snake != null){
-                this.draw(snake, ctx)
-            }
-        });
-    }  
-
-    // -- helper methods -- 
-    createFood(){
-        const x = Math.floor(Math.random()*tc[0]) + 1
-        const y = Math.floor(Math.random()*tc[1]) + 1
-        return {'x': x, 'y': y}; 
-    }
-    checkFood(snake){
-        //// turn into loop
-        this.food.forEach(f => {
-            if(f.x ==  snake.px && f.y ==  snake.py ){
-                snake.tail+=2; 
-                this.food[this.food.indexOf(f)] = this.createFood();
-                console.log(snake.tail)
-            }
-        });
-        
-    }
-    checkDeath(snake){
-        //snake just created
-        if( snake.px ==  snake.py == -5){
-            return true; 
-        }
-
-        //snake hits wall
-        if( snake.px < 1 ||  snake.px > tc[0] 
-            || snake.py < 1|| snake.py > tc[1]){
-            return true; 
-        }
-        return false; 
-    }
-    drawScores(ctx){
-        let text = ""
-        this.players.forEach(snake => {
-            if(snake){
-                ctx.fillStyle = snake.color;
-                text = snake.name + "'s Score: " + (snake.tail-5); 
-                ctx.fillText(text, size*7*(snake.playernum+1) -size*5, size*2); 
-            }
-        })
-
-    }
-
-    // methods to apply on snake
-    respawn(snake){
-        snake.px = 5 * (snake.playernum+1); 
-        snake.py = 5; 
-        snake.tail = 5;
-        snake.vx = snake.vy = 0;
-        snake.trail = []; 
-    }
-    move(snake){
-        snake.px+= snake.vx;
-        snake.py+= snake.vy;
-        for(var i=0; i<snake.trail.length; i++){
-            if(snake.trail[i].x == snake.px && snake.trail[i].y == snake.py ){
-                snake.tail = 5; 
-                this.respawn(snake);
-            }
-        } 
-        snake.trail.push({x: snake.px,y: snake.py}); 
-        while( snake.trail.length> snake.tail){
-            snake.trail.shift()
-        }
-    }
-    draw(snake, ctx){
-        //paint snake
-        ctx.fillStyle= snake.color; 
-        for(let i=0; i<snake.trail.length; i++){
-            ctx.fillRect(snake.trail[i].x*size, snake.trail[i].y*size, size-2, size-2)
-        } 
-    }
-
-};
-
-game  = new Game();  
-socket.on('pn', (pn) => {
-    game.playernum = pn;
-}) 
 
 
 // -- menu screen and setup -- 
@@ -181,13 +89,11 @@ function startGame() {
     playerName = playerNameInput.value.replace(/(<([^>]+)>)/ig, '');
     document.getElementById('gameAreaWrapper').style.display = 'block';
     document.getElementById('startMenuWrapper').style.display = 'none';
-    
-    game.handleNetwork()
     // add player
     socket.emit('addplayer', {playerName: playerName}); //sends json
-   
+
+    gameloop();     
     
-    setInterval(gameloop,1000/5);
 }
 
 // check if nick is valid alphanumeric characters (and underscores)
@@ -221,15 +127,7 @@ window.onload = function() {
     });
 };
 
-// -- GAME LOOP HERE --
-function gameloop(){
-    //console.log(game, socket)
-    window.addEventListener('keydown', getDirection);
-    //game.handleNetwork(socket);
-    game.handleLogic();
-    game.handleGraphics(ctx);
-    game.handleNetwork(socket);
-}
+
 
 
 // -- event listeners --
@@ -242,30 +140,35 @@ window.addEventListener('resize', () => {
     gameRect = [size, size, size*tc[0], size*tc[1]];
 }, true);
 
+
+
 function getDirection(evt){
     //switch direction as long as its not a 180
     console.log('here')
+    let vx = players[playernum].vx
+    let vy = players[playernum].vy
     if(this.playernum != -1){
         switch(evt.keyCode){
             case 37: //left
-                if(game.getMe().vx != 1){
-                    game.getMe().vx=-1; game.getMe().vy=0;
+                if(vx != 1){
+                    vx=-1; vy=0;
                 } break;
             case 38: //up
-                if(game.getMe().vy != 1){
-                    game.getMe().vx=0; game.getMe().vy=-1;
+                if(vy != 1){
+                    vx=0; vy=-1;
                 } break;
             case 39://right
-                if(game.getMe().vx != -1){
-                    game.getMe().vx=1; game.getMe().vy=0; 
+                if(vx != -1){
+                    vx=1; vy=0; 
                 } break;
             case 40: //down
-                if(game.getMe().vy != -1){
-                    game.getMe().vx=0; game.getMe().vy=1;
+                if(vy != -1){
+                    vx=0; vy=1;
                 } break;
         }
     }
     window.removeEventListener('keydown', getDirection); //remove so no runinto self
+    socket.emit('update', {vx: vx, vy: vy, pn: playernum}); 
 };
 
 
--- server/server.js
+++ server/server.js
@@ -5,13 +5,92 @@ const app     = express();
 const http    = require('http').Server(app);
 const io  = require('socket.io')(http);
 const config  = require('./config.json');
-
 const MAX_CONNS = 8; 
 
-const players = {}
-const spots = Array(MAX_CONNS).fill(false);
+const players = Array(MAX_CONNS).fill(null); 
+let food = []
 
-const colors = ["lime", "lightblue", "orange", "purple"]
+// -- OBJECTS FOR THE GAME --
+class Game {
+  constructor(){
+      this.tc = [60, 40];
+      food.push(this.createFood());
+  }
+ // -- helper methods -- 
+  createFood(){
+      const x = Math.floor(Math.random()*this.tc[0]) + 1
+      const y = Math.floor(Math.random()*this.tc[1]) + 1
+      return {'x': x, 'y': y}; 
+  }
+  checkFood(snake){
+      //// turn into loop
+      food.forEach(f => {
+          if(f.x ==  snake.px && f.y ==  snake.py ){
+              snake.tail+=2; 
+              food[food.indexOf(f)] = this.createFood();
+              console.log(snake.tail)
+          }
+      });
+  }
+  checkDeath(snake){
+      //if snake hits wall
+      if( snake.px < 1 ||  snake.px > this.tc[0] 
+          || snake.py < 1|| snake.py > this.tc[1]){
+            this.respawn(snake);
+      }
+      //if snake hits another snake or itself
+      players.forEach((other) => {
+        if(other){
+          for(let i=0; i<other.trail.length; i++){
+              if(other.trail[i].x === snake.px && other.trail[i].y === snake.py ){
+                console.log('dead', snake.px, snake.py);
+                if(other.px === snake.px && other.py === snake.py ) { //check head to head
+                  this.respawn(other);
+                }
+                this.respawn(snake);
+              }
+          }
+        }
+      });
+  }
+  respawn(snake){
+      const x = Math.floor(Math.random()*(this.tc[0]-10))+5
+      const y = Math.floor(Math.random()*(5))+1
+      snake.px = x; 
+      snake.py = 5*y; 
+      snake.tail = 5;
+      snake.vx = 0;
+      snake.vy = 1;
+      snake.trail = []; 
+  }
+};
+
+const game = new Game(); 
+
+// -- GAME LOOP HERE --
+setInterval(handleLogic,1000/5);
+
+  // handle the core functins of the sankes
+function handleLogic() {
+    players.forEach(snake => {
+        if(snake){
+          //move 
+          snake.px+= snake.vx;
+          snake.py+= snake.vy;
+          //check collisions
+          game.checkFood(snake); 
+          game.checkDeath(snake); 
+          //update trail
+          snake.trail.push({x: snake.px,y: snake.py}); 
+          while(snake.trail.length> snake.tail){
+              snake.trail.shift()
+          }
+        }
+    });
+    sendData(); 
+} 
+
+
 
 //setup server
 app.use(express.static(__dirname + '/../client'));
@@ -24,71 +103,62 @@ http.listen(serverPort, () => {
 
 io.on('connection', (socket) => {
   console.log('New connection with id ' +socket.id)
-  socket.emit('pn', playernum())
   socket.on('disconnect', disconnect);
   socket.on('addplayer', addPlayer); 
-  socket.on('datarequest', sendData);    
+  socket.on('update', update);    
 });
 
 //adds info to player 
 function addPlayer(data){
-  let pn = MAX_CONNS;
-  //add player to earliest slot
   for(let i = 0; i < MAX_CONNS; i++){
-    if(!spots[i]){
-      spots[i] = true;
-      pn = i;
+    if(players[i] === null){
+      players[i] = createSnake(data.playerName, this.id);
+      game.respawn(players[i]);
+      food.push(game.createFood());
+      sendData(); 
+      this.emit('pn', i)
       break;
     }
   }
-  players[this.id] = createSnake(data.playerName, pn);
   console.log(players);
-  console.log('in add player');
-  io.emit('data', players); 
+  
+}
+function sendData(){
+  io.emit('data', {players: players, food: food}); 
 }
 
-function playernum(){
-  let pn = MAX_CONNS;
-  //add player to earliest slot
-  for(let i = 0; i < MAX_CONNS; i++){
-    if(!spots[i]){
-      pn = i;
-      break;
-    }
-  }
-  console.log(pn)
-  return pn; 
-}
 
 //disconnect a player
 function disconnect(){
-  if(players[this.id]){
-    spots[players[this.id].playernum] = false; //set slot to open
-    delete players[this.id];
-  }
-  console.log('A user disconnected');
-  console.log(players)
+  for(let i = 0; i < players.length; i++){
+    if(players[i]){
+      //console.log('client', client.id, this.id); 
+      if(players[i].id === this.id){ 
+        console.log('A user disconnected with id: ', players[i].id);
+        players[i] = null;
+        break; 
+      }
+    }
+  } 
+  //console.log(players)
 }; 
 
-function sendData(data){
-  players[this.id] = data; //update players with data that socket sends
-  io.emit('data', players); 
+function update(data){
+  players[data.pn].vx = data.vx; //update players with data that socket sends
+  players[data.pn].vy = data.vy;
+  console.log(players); 
 }
 
-
-
-
-
-function createSnake(name, playernum){
+function createSnake(name, id){
   return ({
     name: name, 
-    playernum: playernum, 
-    color: colors[playernum],
+    id: id, 
     px: 0,
     py: 0,
     vx: 0, 
     vy: 0,
-    tail: 1, 
+    tail: 5, 
     trail: []
   });
 }
+
